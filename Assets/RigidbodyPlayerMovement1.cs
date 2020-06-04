2e13aa8f33f50614003d7ae0b2d74684cb940304
Nathan Perry <nateperry333@gmail.com>
2020-06-04 02:09:59 -0400

    Separates out horizontal and vertical movement. Adds speed cap (incomplete)

--- Assets/RigidbodyPlayerMovement1.cs
+++ Assets/RigidbodyPlayerMovement1.cs
@@ -6,7 +6,9 @@ public class RigidbodyPlayerMovement1 : MonoBehaviour
 {
     public GameObject directionObject;
     public Rigidbody targetRigidbody;
-    public float speed = 10.0f;
+    public float speed = 1000.0f;
+    public float maxSpeed = 15.0f;
+    public float maxVelocity = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,14 +25,38 @@ public class RigidbodyPlayerMovement1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         float moveHorizontal = Input.GetAxis("Horizontal");
+        targetRigidbody.AddForce(directionObject.transform.right * moveHorizontal * speed * Time.deltaTime);
+
         float moveVertical = Input.GetAxis("Vertical");
+        // targetRigidbody.AddForce(directionObject.transform.forward * moveVertical * speed * Time.deltaTime);
+        targetRigidbody.AddForce(directionObject.transform.forward * moveVertical * speed * Time.deltaTime);
 
-        // Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        Vector3 movement = (directionObject.transform.right * moveHorizontal) + (directionObject.transform.forward * moveVertical);
+        // UpdateMaxSpeed();
+    }
 
-        // characterController.Move((directionObject.transform.right * moveHorizontal + directionObject.transform.forward * moveVertical) * Time.deltaTime);
-        targetRigidbody.AddForce(movement * speed * Time.deltaTime);
+    void UpdateMaxSpeed()
+    {
+        // Based on https://answers.unity.com/questions/9985/limiting-rigidbody-velocity.html
+
+        if (maxSpeed != 0.0f) {
+            // ##### Method 1
+            // float speed = Vector3.Magnitude(targetRigidbody.velocity);  // test current object speed
+            // if (speed > maxSpeed) {
+            //     float brakeSpeed = speed - maxSpeed;  // calculate the speed decrease
+            //     Vector3 brakeVelocity = targetRigidbody.velocity.normalized * brakeSpeed;  // make the brake Vector3 value
+            //     targetRigidbody.AddForce(-brakeVelocity);  // apply opposing brake force
+            // }
+            // ##### Method 1
+
+            // ##### Method 2
+            // if (targetRigidbody.velocity.sqrMagnitude > maxVelocity)
+            // {
+            //     //smoothness of the slowdown is controlled by the 0.99f, 
+            //     //0.5f is less smooth, 0.9999f is more smooth
+            //     targetRigidbody.velocity *= 0.99f;
+            // }
+            // ##### Method 2
+        }
     }
 }
